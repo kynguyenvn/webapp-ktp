2b331ee7241a25f47b528946cd0b7a689d2cb8cc
kynguyenvn <kynguyen92bkdn@gmail.com>
2022-08-12 14:14:41 +0700

    feat:add eslint ,husky

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		semi: 0,
+		'comma-dangle': 0,
+		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+	},
+	plugins: ['react', 'prettier'],
+};
